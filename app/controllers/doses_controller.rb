f409439cc2f50c0349bcfea8a0eb47ce6cf29602
disha patel <disha.p@hotmail.co.uk>
2016-08-19 13:38:04 +0100

    dose form etc working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,17 +7,18 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    @dose = @cocktail.doses.build(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@cocktail)
 
   end
 
